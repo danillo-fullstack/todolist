a495edc4a3ef7c4865a87ef9443f134ccf7e661e
Danillo Victtor <danilo_victor2004@hotmail.com>
2025-09-29 09:15:36 -0300

    Adiciona o ID do usuário como um atributo no request.

--- src/main/java/br/com/danillo/todolist/task/TaskModel.java
+++ src/main/java/br/com/danillo/todolist/task/TaskModel.java
@@ -8,6 +8,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
@@ -16,7 +17,7 @@ import lombok.Data;
 public class TaskModel {
 
     @Id
-    @GeneratedValue(generator = "UUID")
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @Column(length = 50)
